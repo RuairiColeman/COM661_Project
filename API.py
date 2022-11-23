015f39ddf0fd39855ba154d6a013eca27cd4959c
Ruairi <coleman-r@ulster.ac.uk>
2022-11-23 10:57:55 +0000

    Added GET functions for Movies, Series & single titles

--- API.py
+++ API.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from flask import Flask, request, jsonify, make_response
 from pymongo import MongoClient
 
@@ -32,27 +33,59 @@ def show_all_titles():
 
 @app.route("/api/v1.0/titles/<string:id>", methods=["GET"])
 def show_one_title(id):
-    return 0
+    title = media.aggregate([{"$match": {"_id": ObjectId(id)}}])
+
+    if title is not None:
+        for title in media.aggregate(pipeline=[{"$project": {"_id": 0}}, ]):
+            for review in title["reviews"]:
+                review["_id"] = str(review["_id"])
+            return make_response(jsonify(title), 200)
+    else:
+        return make_response(jsonify({"error": "Invalid business ID"}), 404)
 
 
 @app.route("/api/v1.0/movies", methods=["GET"])
 def show_all_movies():
-    return 0
+    page_num, page_size = 1, 10
+    if request.args.get('pn'):
+        page_num = int(request.args.get('pn'))
+    if request.args.get('ps'):
+        page_size = int(request.args.get('ps'))
+    page_start = (page_size * (page_num - 1))
 
+    data_to_return = []
+    pipeline = [
+        {"$project": {"title": 1, "_id": 0, "type": "Movie"}},
+        {"$skip": page_start},
+        {"$limit": page_size}
+    ]
 
-@app.route("/api/v1.0/movies/<string:id>", methods=["GET"])
-def show_one_movie(id):
-    return 0
+    for title in media.aggregate(pipeline):
+        data_to_return.append(title)
+
+    return make_response(jsonify(data_to_return), 200)
 
 
 @app.route("/api/v1.0/series", methods=["GET"])
 def show_all_series():
-    return 0
+    page_num, page_size = 1, 10
+    if request.args.get('pn'):
+        page_num = int(request.args.get('pn'))
+    if request.args.get('ps'):
+        page_size = int(request.args.get('ps'))
+    page_start = (page_size * (page_num - 1))
 
+    data_to_return = []
+    pipeline = [
+        {"$project": {"title": 1, "_id": 0, "type": "TV Show"}},
+        {"$skip": page_start},
+        {"$limit": page_size}
+    ]
 
-@app.route("/api/v1.0/series/<string:id>", methods=["GET"])
-def show_one_series(id):
-    return 0
+    for title in media.aggregate(pipeline):
+        data_to_return.append(title)
+
+    return make_response(jsonify(data_to_return), 200)
 
 
 if __name__ == "__main__":
