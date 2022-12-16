46096effc02fc93b3b05da314924ae0f826df75b
Ruairi <coleman-r@ulster.ac.uk>
2022-12-16 11:14:43 +0000

    removed unnecessary code

--- API.py
+++ API.py
@@ -17,67 +17,6 @@ app.config['SECRET_KEY'] = 'mysecret'
 client = MongoClient("mongodb://127.0.0.1:27017")
 db = client.mydb
 media = db.finalnotflix
-users = db.users
-blacklist = db.blacklist
-
-
-def jwt_required(func):
-    @wraps(func)
-    def jwt_required_wrapper(*args, **kwargs):
-        token = None
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-        if not token:
-            return jsonify({'message': 'Token is missing'}), 401
-        try:
-            jwt.decode(token, app.config['SECRET_KEY'])
-        except:
-            return jsonify({'message': 'Token is invalid'}), 401
-
-        return func(*args, **kwargs)
-
-    return jwt_required_wrapper
-
-
-@app.route('/api/v1.0/login', methods=['GET'])
-def login():
-    auth = request.authorization
-    if auth:
-        user = users.find_one({'username': auth.username})
-        if user is not None:
-            if bcrypt.checkpw(bytes(auth.password, 'UTF-8'), user["password"]):
-                token = jwt.encode(
-                    {'user': auth.username,
-                     'admin': user["admin"],
-                     'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
-                     }, app.config['SECRET_KEY'])
-                return make_response(jsonify({'token': token.decode('UTF-8')}), 200)
-            else:
-                return make_response(jsonify({'message': 'Bad password'}), 401)
-        else:
-            return make_response(jsonify({'message': 'Bad username'}), 401)
-    return make_response(jsonify({'message': 'Authentication required'}), 401)
-
-
-@app.route('/api/v1.0/logout', methods=["GET"])
-@jwt_required
-def logout():
-    token = request.headers['x-access-token']
-    blacklist.insert_one({"token": token})
-    return make_response(jsonify({'message': 'Logout successful'}), 200)
-
-
-def admin_required(func):
-    @wraps(func)
-    def admin_required_wrapper(*args, **kwargs):
-        token = request.headers['x-access-token']
-        data = jwt.decode(token, app.config['SECRET_KEY'])
-        if data["admin"]:
-            return func(*args, **kwargs)
-        else:
-            return make_response(jsonify({'message': 'Admin access required'}), 401)
-
-    return admin_required_wrapper
 
 
 @app.route("/api/v1.0/titles", methods=["GET"])
